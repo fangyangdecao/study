e7e94e368e273b5c67b0d4baa87f0284ee99bbe8
yanhuayang <1260504734@qq.com>
2020-07-13 10:10:40 +0800

    test lettuce redis

--- base/src/main/java/com/yhy/controller/TestController.java
+++ base/src/main/java/com/yhy/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.yhy.controller;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,5 +15,18 @@ public class TestController {
     @GetMapping("hello")
     public String test(@RequestParam String name){
         return "hello" + name;
+    }
+
+
+    @GetMapping("redis")
+    public void redis(@RequestParam String name){
+        RedisClient redisClient = RedisClient.create("redis://192.168.146.135:6379/0");
+        StatefulRedisConnection<String, String> connect = redisClient.connect();
+        RedisCommands<String, String> sync = connect.sync();
+        sync.set(name,"hello");
+        sync.setex(name+1,5,"expired");
+        sync.ttl("test");
+        connect.close();
+        redisClient.shutdown();
     }
 }
